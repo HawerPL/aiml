35e76830014ac28ff68a532581644235e512aa83
HawerPL <hubertsob217@gmail.com>
2022-10-09 21:14:00 +0200

    [HS] Obługa rejestracji z bazą danych.

--- app.py
+++ app.py
@@ -1,18 +1,49 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
-from wtforms.validators import DataRequired, InputRequired, EqualTo, Email
-
-#from flask_login import LoginManager
+from wtforms.validators import DataRequired, Email, EqualTo
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import Integer, String, Date, Boolean, MetaData
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret.key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/aiml'
+db = SQLAlchemy(app)
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column('u_id', Integer, primary_key=True)
+    username = db.Column('u_username', String, unique=True)
+    email = db.Column('u_email', String, unique=True)
+    password_hash = db.Column('u_password_hash', String)
+    is_active = db.Column('u_active', Boolean)
+    created = db.Column('u_created', Date, default=datetime.now())
+    last_update = db.Column('u_last_update', Date)
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "User(username='{}', email='{}', password_hash='{}', is_active='{}', created='{}', last_update='{}')"\
+            .format(self.username, self.email, self.password_hash, self.is_active, self.created, self.last_update)
 
 
 class LoginForm(FlaskForm):
@@ -23,8 +54,8 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[DataRequired()])
-    password = PasswordField([InputRequired(), EqualTo('confirmPassword', message='Passwords must match')])
-    confirmPassword = PasswordField()
+    password = PasswordField(validators=[DataRequired(), EqualTo('password', message="Hasła muszą być identyczne!")])
+    confirmPassword = PasswordField(validators=[DataRequired()])
     email = EmailField(validators=[DataRequired(), Email()])
     registerSubmit = SubmitField()
 
@@ -56,7 +87,12 @@ def register():
         username = form.username.data
         password = form.password.data
         email = form.email.data
-        form.username.data, form.password.data, form.email.data = ''
+        if User.query.filter_by(email=email).first() is None or User.query.filter_by(username=username).first() is None:
+            user = User(username=username, email=email, password_hash=generate_password_hash(password))
+            db.session.add(user)
+            db.session.commit()
+        form.username.data, form.password.data, form.email.data, form.confirmPassword = '', '', '', ''
+
     return render_template('register.html',
                            username=username,
                            password=password,
@@ -67,15 +103,18 @@ def register():
 @app.errorhandler(404)
 def page_not_found(e):
     app.logger.error(e)
-    return render_template('404.html', error=e), 404
+    return render_template('404.html', error=e,  error_code="404"), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
     app.logger.error(e)
-    return render_template('500.html', error=e), 500
+    return render_template('500.html', error=e, error_code="500"), 500
+
+
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
     app.run()
-
